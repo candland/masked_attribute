7d6eed119c3446b598a527f1c2bc0e26da084cfe
candland <candland@gmail.com>
2024-01-15 11:11:33 -0700

    Fix with_roles hard coded. Bump version

--- lib/masked_attribute.rb
+++ lib/masked_attribute.rb
@@ -53,16 +53,16 @@ module MaskedAttribute
       indexed_masks = masks.map { |v| [2**masks.index(v), v] }.to_h.freeze
 
       const_set masks_const_name, masks
-      const_set "INDEXED_#{masks_const_name}", indexed_masks
+      const_set :"INDEXED_#{masks_const_name}", indexed_masks
 
       # Add a scope with_ATTRIBUTE_NAME, which returns records that match ALL given masks
-      self.class.define_method("with_#{attribute_name}") do |*values|
+      self.class.define_method(:"with_#{attribute_name}") do |*values|
         ok_mask = mask_from(masks, values)
         where("role_mask & ? = ?", ok_mask, ok_mask)
       end
 
       # Add a scope with_any_ATTRIBUTE_NAME, which returns records that match ANY given masks
-      self.class.define_method("with_any_#{attribute_name}") do |*values|
+      self.class.define_method(:"with_any_#{attribute_name}") do |*values|
         ok_mask = mask_from(masks, values)
         where("role_mask & ? != 0", ok_mask)
       end
@@ -71,31 +71,31 @@ module MaskedAttribute
         value_int = 2**masks.index(value)
 
         # Add MASK? method. Returns true if the mask is set
-        define_method("#{value}?") do
+        define_method(:"#{value}?") do
           has = __send__(mask_attribute_name)
           has & value_int > 0
         end
 
         # Add add_MASK! method. Updates the MASKS_mask with the MASK in the name
-        define_method("add_#{value}!") do
+        define_method(:"add_#{value}!") do
           has = __send__(mask_attribute_name)
           update!("#{mask_attribute_name}": (has | value_int))
         end
 
         # Add remove_MASK! method. Updates the MASKS_mask without the MASK in the name
-        define_method("remove_#{value}!") do
+        define_method(:"remove_#{value}!") do
           has = __send__(mask_attribute_name)
           update!("#{mask_attribute_name}": (has & ~value_int))
         end
 
         # Add scope MASK, to return records with the MASK in the name
-        scope value.to_s.pluralize, -> { with_roles(value) }
+        scope value.to_s.pluralize, -> { __send__(:"with_#{attribute_name}", value) }
       end
 
       # Add attr_writer for the attribute_name
-      define_method("#{attribute_name}=") do |value|
+      define_method(:"#{attribute_name}=") do |value|
         value = Array.wrap(value).map(&:to_sym)
-        __send__("#{mask_attribute_name}=", self.class.mask_from(masks, value))
+        __send__(:"#{mask_attribute_name}=", self.class.mask_from(masks, value))
       end
 
       # Add attr_reader for the attribute_name
@@ -107,18 +107,18 @@ module MaskedAttribute
 
       # Add a MASK from the mask_attribute_name
       # params: value = the mask to add
-      define_method("add_#{attribute_name.singularize}") do |value|
+      define_method(:"add_#{attribute_name.singularize}") do |value|
         value_int = 2**masks.index(value)
         has = __send__(mask_attribute_name)
-        __send__("#{mask_attribute_name}=", has | value_int)
+        __send__(:"#{mask_attribute_name}=", has | value_int)
       end
 
       # Remove a MASK from the mask_attribute_name
       # params: value = the mask to remove
-      define_method("remove_#{attribute_name.singularize}") do |value|
+      define_method(:"remove_#{attribute_name.singularize}") do |value|
         value_int = 2**masks.index(value)
         has = __send__(mask_attribute_name)
-        __send__("#{mask_attribute_name}=", has & ~value_int)
+        __send__(:"#{mask_attribute_name}=", has & ~value_int)
       end
     end
   end
